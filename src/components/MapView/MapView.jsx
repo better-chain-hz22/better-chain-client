5072e90c14fb2628f4f52e89542630cb94c8ec30
cslylla <81328857+cslylla@users.noreply.github.com>
2022-09-17 15:24:17 +0200

    Add data

--- src/components/MapView/MapView.jsx
+++ src/components/MapView/MapView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import {MapContainer, TileLayer} from "react-leaflet";
 import './map_view.scss';
 import dataPoints from './points.json';
@@ -6,6 +7,11 @@ import {mock_data} from "./mock";
 import MapViewFunctions from "./MapViewFunctions";
 
 function MapView({isShowPorts}) {
+    const navigate = useNavigate();
+    const navToOrder = () => {
+        navigate('/order-number');
+    }
+
     return (
             <MapContainer center={dataPoints.distributionCenter}
                           zoom={3}
@@ -13,6 +19,7 @@ function MapView({isShowPorts}) {
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    onClick={navToOrder}
                 />
                 {isShowPorts && MapViewFunctions.renderDestinationPorts(mock_data.destination_ports)}
                 {isShowPorts && MapViewFunctions.renderSourcePorts(mock_data.source_ports)}
